df8f13f2b145404ea03c2180f8466a58ad837331
Max Tsymba <max.tsymbaliuk@data-ox.com>
2022-06-29 00:16:55 +0300

    config: init project

---
+++ client/app/js/main.js
@@ -0,0 +1,9 @@
+import api from './api/api';
+import helper from './utils/helpers';
+
+/*======================================DOM LOADED======================================*/
+window.addEventListener('DOMContentLoaded', () => {
+  api();
+  helper();
+});
+/*======================================================================================*/
